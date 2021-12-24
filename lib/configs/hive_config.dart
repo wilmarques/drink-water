3a56206d6368c1bb4774397d90c9fb52ed76e7d4
wilmarques <wileymarques@live.com>
2021-12-24 17:52:26 +0000

    refactor: Migrating from Shared Prefs to Hive

--- lib/configs/hive_config.dart
+++ lib/configs/hive_config.dart
@@ -1,7 +1,11 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveConfig {
+
+  static String waterIntakeBoxName = 'water_intake';
+
   static start() async {
     await Hive.initFlutter();
+    await Hive.openBox(waterIntakeBoxName);
   }
 }
--- lib/water_intake_storage.dart
+++ lib/water_intake_storage.dart
@@ -1,14 +1,12 @@
-// import 'package:hive/hive.dart';
+import 'package:hive/hive.dart';
+
+import 'configs/hive_config.dart';
 
 class WaterIntakeStorage {
-  // late Box box;
+  late final Box box;
 
   WaterIntakeStorage() {
-    _initialize();
-  }
-
-  Future<void> _initialize() async {
-    // box = await Hive.openBox('water_intake');
+    box = Hive.box(HiveConfig.waterIntakeBoxName);
   }
 
   int getWaterIntake() {
@@ -19,24 +17,20 @@ class WaterIntakeStorage {
       return 0;
     }
 
-    // return box.get('intake', defaultValue: 0);
-    return 0;
+    return box.get('intake', defaultValue: 0);
   }
 
   Future<void> storeWaterIntake(int value) async {
     _saveLastEditDate();
-    // return await box.put('intake', value);
-    return Future.value();
+    return await box.put('intake', value);
   }
 
   int _getLastEditDate() {
-    // return box.get('last_edit_date', defaultValue: 0);
-    return 16;
+    return box.get('last_edit_date', defaultValue: 0);
   }
 
   Future<void> _saveLastEditDate() async {
     final todayDay = DateTime.now().day;
-    // return await box.put('last_edit_date', todayDay);
-    return Future.value();
+    return await box.put('last_edit_date', todayDay);
   }
 }
